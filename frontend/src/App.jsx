b2a2abb8df3491e2731fb4f573c77aed63a5907f
guptab8 <guptab8@mcmaster.ca>
2025-01-11 22:34:00 -0500

    Add assets and header files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,37 +1,46 @@
-import { useState, useEffect } from 'react'
-import './App.css'
+// import { useState, useEffect } from "react";
+import ButtonGradient from "./assets/svg/ButtonGradient";
+import Button from "./components/Button";
+import Header from "./components/Header";
 
+const App = () => {
+  return (
+    <>
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Header />
+      </div>
+      <ButtonGradient />
+    </>
+  );
+};
 
+/* 
 function App() {
-  const [data, setData] = useState([])
+  const [data, setData] = useState([]);
 
-  useEffect(()=> {
+  useEffect(() => {
     async function fetchData() {
-      console.log(import.meta.env.VITE_API_URL)
+      console.log(import.meta.env.VITE_API_URL);
       try {
         const response = await fetch(`${import.meta.env.VITE_API_URL}posts`);
         if (!response.ok) {
-          throw new Error('Network response was not ok');
+          throw new Error("Network response was not ok");
         }
         const result = await response.json();
-        console.log(result)
+        console.log(result);
         setData(result);
       } catch (error) {
-        console.error('Error fetching data', error);
+        console.error("Error fetching data", error);
       }
     }
     fetchData();
-    
-  }, [])
+  }, []);
 
   return (
     <>
-     {/* <Home /> */}
-     <h1 className="text-3xl font-bold underline">
-        CODECLASH
-     </h1>
+      <h1 className="text-3xl font-bold underline">CODECLASH</h1>
     </>
-  )
-}
+  );
+} */
 
-export default App
+export default App;
